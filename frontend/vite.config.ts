0a6965762293d6cf1491dbae33db3c470cee9797
lm379 <2645081603@qq.com>
2026-01-10 18:51:59 +0800

    feat: 添加 Rollup 输出配置以优化构建过程

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -16,6 +16,23 @@ export default defineConfig({
   },
   build: {
     outDir: '../cmd/server/static', 
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+        entryFileNames: 'js/[name]-[hash].js',
+        chunkFileNames: 'js/[name]-[hash].js',
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name && assetInfo.name.endsWith('.css')) {
+            return 'css/[name]-[hash][extname]';
+          }
+          return 'assets/[name]-[hash][extname]';
+        }
+      }
+    }
   }
 })
